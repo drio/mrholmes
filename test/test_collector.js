8ca9ccba4ccd26cbd7f5f065dd56a4752fe09765
David Rio Deiros <driodeiros@gmail.com>
2013-10-04 15:08:56 -0400

    Testing the collector

--- test/test_collector.js
+++ test/test_collector.js
@@ -6,7 +6,23 @@ var path     = require('path'),
     assert   = require('assert'),
     mrholmes = require(lib + '/mrholmes');
 
-assert(true, 'Great');
+function test_array_of_files(data) {
+  var t_files = { 'test/test_dir/a_link/foo.txt' : true,
+                  'test/test_dir/dir1/foo.txt' : true,
+                  'test/test_dir/file1.txt' : true };
 
+  assert(data.length === 3);
+  data.forEach(function(e, i, c) {
+    assert(t_files[e.path]);
+    assert(e.file === 'f');
+  });
+}
+
+mrholmes.collector()
+        .this_dirs(['test/test_dir'])
+        .run(function(data) {
+          console.log(data);
+          test_array_of_files(data);
+        });
 
 
