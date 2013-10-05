aea5c3f953f80a85019033eefc217346447bb4cf
David Rio Deiros <driodeiros@gmail.com>
2013-10-05 09:50:06 -0400

    Propagating interface changes to the collect binary

--- bin/mrh-collect.js
+++ bin/mrh-collect.js
@@ -3,11 +3,16 @@
 var path     = require('path'),
     fs       = require('fs'),
     lib      = path.join(path.dirname(fs.realpathSync(__filename)), '../lib');
-    mrholmes = require(lib + '/mrholmes');
+    mrholmes = require(lib + '/collector');
 
-var t = process.hrtime();
-mrholmes.collector().run(function(data) {
+var t = process.hrtime(),
+    collector = mrholmes.collector(),
+    pf_engine = collector.pf_engines.process_file_tree;
+
+function ready(data) {
   process.stderr.write('>> # of files: ' + data.length + "\n");
-  console.log(JSON.stringify(data));
+  console.log(JSON.stringify(data, null, 2));
   process.stderr.write('>> Elapse time: ' + process.hrtime(t)[0] + " secs\n");
-});
+}
+
+collector.run(ready, pf_engine);
