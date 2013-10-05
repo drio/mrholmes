8ed5dc94e1d525b55d3fd885040f1ec5c9750945
David Rio Deiros <driodeiros@gmail.com>
2013-10-05 09:35:07 -0400

    Adding more tree engine testing.

--- test/test_collector.js
+++ test/test_collector.js
@@ -6,26 +6,66 @@ var path     = require('path'),
     assert   = require('assert'),
     co = require(lib + '/collector');
 
-function test_array_of_files(data) {
-  var t_files = { 'test/test_dir/a_link/foo.txt' : true,
-                  'test/test_dir/dir1/foo.txt' : true,
-                  'test/test_dir/file1.txt' : true };
-
-  assert(data.length === 3);
-  data.forEach(function(e, i, c) {
-    assert(t_files[e.path]);
-    assert(e.file === 'f');
-  });
+function pretty(o) {
+  console.log(JSON.stringify(o, null, 2));
 }
 
-var collector = co.collector(),
-    pf_engine = collector.pf_engines.process_file_array;
+function test_array_of_files() {
+  function t_array_of_files(data) {
+    var t_files = { 'test/test_dir/a_link/foo.txt' : true,
+                    'test/test_dir/dir1/foo.txt' : true,
+                    'test/test_dir/file1.txt' : true };
 
-collector
-  .this_dirs(['test/test_dir'])
-  .run(function(data) {
-    console.log(data);
-    test_array_of_files(data);
-  }, pf_engine);
+    assert(data.length === 3);
+    data.forEach(function(e, i, c) {
+      assert(t_files[e.path]);
+      assert(e.file === 'f');
+    });
+  }
+
+  var collector = co.collector(),
+      pf_engine = collector.pf_engines.process_file_array;
+
+  collector
+    .this_dirs(['test/test_dir'])
+    .run(function(data) {
+      //console.log(data);
+      t_array_of_files(data);
+    }, pf_engine);
+}
+
+function test_tree() {
+  var collector = co.collector(),
+      pf_engine = collector.pf_engines.process_file_tree;
+
+  function t_tree(t) {
+    pretty(t);
+    // /
+    assert(t.name === "/", "testing root failed");
+    assert(t.children.length === 1, "root has the wrong number of nodes");
+
+    // cd test
+    t = t.children[0];
+    assert(t.name === "test_dir", "no level1 dir: test_dir");
+    assert(t.children.length === 3, "level1 wrong # of nodes");
+
+    // cd test/dir1
+    t = t.children;
+    _ = { 'a_link':true, 'dir1':true, 'file1.txt':true };
+    t.forEach(function(e, i, a) {
+      assert(_[e.name], e + "node should not be in test/dir");
+    });
+  }
+
+  collector
+    .this_dirs(['test/test_dir'])
+    .run(function(data) {
+      //pretty(data);
+      t_tree(data);
+    }, pf_engine);
+}
+
+test_array_of_files();
+test_tree();
 
 
